a63aaa3156a0f1c97ceb1ffcceccc5c8ca36c251
Pawlo Picasso <Quaslusive@protonmail.com>
2024-04-27 00:21:06 +0200

    v0.1

--- js/main.js
+++ js/main.js
@@ -20,54 +20,6 @@ btnTopRank.addEventListener('click', () => {
 });
 
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const searchForm = document.getElementById('searchForm');
-    const searchInput = document.getElementById('searchInput');
-    const searchSelect = document.querySelector('select');
-    const searchResultsContainer = document.getElementById('searchResults');
-
-    searchForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-
-        const query = searchInput.value;
-        const searchType = searchSelect.value;
-
-        try {
-            let results;
-            if (searchType === 'movie') {
-                results = await searchMovies(query);
-            } else if (searchType === 'people') {
-                results = await searchPeople(query);
-            } else {
-                throw new Error('Invalid search type');
-            }
-
-            displaySearchResults(results);
-        } catch (error) {
-            console.error('Error searching:', error.message);
-        }
-    });
-
-    function displaySearchResults(results) {
-
-
-
-
-        const searchResultsDiv = document.createElement('div');
-
-
-        results.forEach(result => {
-            const resultElement = document.createElement('div');
-            resultElement.textContent = result.title || result.name; // Use title for movies, name for people
-            searchResultsDiv.appendChild(resultElement);
-        });
-
-        resultContainer.appendChild(searchResultsDiv);
-    }
-});
-
-
 /*
 
 const errorMessage = document.querySelector('#errorMessage');
